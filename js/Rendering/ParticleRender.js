b3e73a27fef3989d5d842fcbd8634b66a0afc9a3
Bewelge <17884995+Bewelge@users.noreply.github.com>
2021-01-10 16:07:15 +0100

    improved Particle performance

--- js/Rendering/ParticleRender.js
+++ js/Rendering/ParticleRender.js
@@ -4,75 +4,62 @@ export class ParticleRender {
 		this.particles = new Map()
 	}
 	createParticles(x, y, w, h, color) {
-		for (let i = 0; i < Math.random() * 40; i++) {
+		for (let i = 0; i < Math.random() * this.settings.particleAmount; i++) {
 			let rndX = x + w * 0.5 + w * 0.5 * (Math.random() - Math.random()) // / 2 - (w / 2) * (Math.random() - Math.random())
 
-			let ang = (Math.random() - Math.random()) * 0.5 * Math.PI + Math.PI * 1.5
-			let radius = Math.random() * 2 + 0.5
+			let motX = (Math.random() - Math.random()) * this.settings.particleSpeed
+			let motY = -Math.random() * this.settings.particleSpeed
+			let radius = Math.random() * this.settings.particleSize + 0.5
 			rndX -= radius / 2
-			let motion = Math.random() * 7 + 0.5
-			let angMotion = Math.random() * 0.05
-			let lifeTime = Math.random() * 3 + 5
-			this.createParticle(
-				rndX,
-				y,
-				ang,
-				radius,
-				motion,
-				angMotion,
-				color,
-				lifeTime
-			)
+			let lifeTime = Math.random() * this.settings.particleLife + 2
+			this.createParticle(rndX, y, motX, motY, radius, color, lifeTime)
 		}
 	}
-	createParticle(x, y, ang, radius, motion, angMotion, color, lifeTime) {
+	createParticle(x, y, motX, motY, radius, color, lifeTime) {
 		if (!this.particles.has(color)) {
 			this.particles.set(color, [])
 		}
-		this.particles
-			.get(color)
-			.push([x, y, ang, radius, motion, angMotion, lifeTime])
+		this.particles.get(color).push([x, y, motX, motY, radius, lifeTime])
 	}
 	updateParticles() {
 		this.particles.forEach(particleArray =>
 			particleArray.forEach(particle => this.updateParticle(particle))
 		)
+		this.clearDeadParticles()
+	}
+	clearDeadParticles() {
 		this.particles.forEach((particleArray, color) => {
 			for (let i = particleArray.length - 1; i >= 0; i--) {
-				if (particleArray[i][6] < 0) {
+				if (particleArray[i][5] < 0) {
 					particleArray.splice(i, 1)
 				}
 			}
-
 			if (particleArray.length == 0) {
 				this.particles.delete(color)
 			}
 		})
 	}
+
 	updateParticle(particle) {
-		particle[0] += Math.cos(particle[2]) * particle[4] //x
-		particle[1] += Math.sin(particle[2]) * particle[4] //y
+		particle[0] += particle[2]
+		particle[1] += particle[3]
 
-		//turn ang
-		particle[2] += particle[5]
-
-		//dampen angular and xy-motion
-		particle[4] *= 0.92
-		particle[5] *= 0.92
-
-		particle[3] = Math.max(0.5, particle[3] * 0.95)
+		//dampen xy-motion
+		particle[2] *= 0.92
+		particle[3] *= 0.92
 
 		//particle lifetime
-		particle[6]--
+		particle[5]--
 	}
-	render() {
+	render(settings) {
+		this.settings = settings
 		this.updateParticles()
 		this.strokeStyle = "rgba(255,255,255,0.2)"
 		this.ctx.globalAlpha = 0.5
 
 		this.particles.forEach((particleArray, color) => {
-			this.ctx.fillStyle = color
 			let c = this.ctx
+			c.fillStyle = color
 			c.beginPath()
 			particleArray.forEach(particle => this.renderParticle(particle))
 			c.fill()
@@ -82,6 +69,6 @@ export class ParticleRender {
 	}
 	renderParticle(particle) {
 		this.ctx.moveTo(particle[0], particle[1])
-		this.ctx.arc(particle[0], particle[1], particle[3], 0, Math.PI * 2, 0)
+		this.ctx.arc(particle[0], particle[1], particle[4], 0, Math.PI * 2, 0)
 	}
 }
