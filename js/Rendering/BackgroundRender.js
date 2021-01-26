3b98448898ac2b78a369f68f204ec6554a03e56d
Bewelge <17884995+Bewelge@users.noreply.github.com>
2021-01-26 18:38:30 +0100

    made background darker

--- js/Rendering/BackgroundRender.js
+++ js/Rendering/BackgroundRender.js
@@ -17,15 +17,15 @@ export class BackgroundRender {
 			this.renderDimensions.windowWidth,
 			this.renderDimensions.windowHeight
 		)
-		const col1 = "rgba(200,200,200,1)"
-		const col3 = "rgba(160,160,160,0.8)"
-		const col2 = "rgba(140,140,140,0.8)"
+		const col1 = "rgba(25,25,25,1)"
+		const col3 = "rgba(50,50,50,1)"
+		const col2 = "rgba(40,40,40,0.8)"
 		c.strokeStyle = col1
 		c.fillStyle = col2
 		let whiteKey = 0
 		for (let i = 0; i < 88; i++) {
 			if (!isBlack(i)) {
-				c.strokeStyle = i % 2 ? col3 : col3
+				c.strokeStyle = i % 2 ? col1 : col1
 				c.fillStyle = (i + 2) % 2 ? col3 : col2
 				c.lineWidth = 1
 				//c.globalAlpha = 0.25  + (i+9)%3 / 4  + (i + 9) % 12 / 48
@@ -34,7 +34,7 @@ export class BackgroundRender {
 				c.strokeRect(dim.x, dim.y, dim.w, this.renderDimensions.windowHeight)
 
 				if (1 + (whiteKey % 7) == 3) {
-					c.lineWidth = 5
+					c.lineWidth = 2
 					c.beginPath()
 					c.moveTo(dim.x, 0)
 					c.lineTo(dim.x, this.renderDimensions.windowHeight)
